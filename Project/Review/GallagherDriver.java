389470860ec977e1edcf623597f7ac8b1d259a10
ManWithNoName <warpd.hazzrd@gmail.com>
2024-01-15 21:34:57 -0500

    jg

--- Project/Review/GallagherDriver.java
+++ Project/Review/GallagherDriver.java
@@ -1,5 +1,22 @@
 package Project.Review;
 
+import java.io.*;
+import java.util.*;
+
 public class GallagherDriver {
-    
+    public static void main(String[] args) {
+
+        ArrayList<Arena> arenas = new ArrayList<>();
+        File text - new File()
+        try {
+            Scanner scanner = new Scanner(new File("IndoorArenas.txt"));
+            while (scanner.hasNextLine()) {
+                
+            }
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }    
 }
